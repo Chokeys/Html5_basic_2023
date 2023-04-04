d910bcca6ab24e126c957ed0d7dccf9d189d5cd6
chokey <gngntkwk@naver.com>
2023-04-04 14:49:50 +0900

    4월4일

---
+++ Day10/js/chart-area-demo.js
@@ -0,0 +1,55 @@
+// Set new default font family and font color to mimic Bootstrap's default styling
+// Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontFamily = '"IBM Plex Sans KR", sans-serif';
+Chart.defaults.global.defaultFontColor = '#292b2c';
+
+// Area Chart Example
+var ctx = document.getElementById("myAreaChart");
+var myLineChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: ["3월1일", "3월2일", "3월3일", "3월4일", "3월5일", "3월6일", "3월7일", "3월8일", "3월9일", "3월10일", "3월11일", "3월12일", "3월13일"],
+    datasets: [{
+      label: "Sessions",
+      lineTension: 0.3,
+      backgroundColor: "rgba(0,0,225,0.5)",
+      borderColor: "rgba(0,0,0,0.5)",
+      pointRadius: 5,
+      pointBackgroundColor: "rgba(2,117,216,1)",
+      pointBorderColor: "rgba(255,255,255,0.8)",
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: "rgba(2,117,216,1)",
+      pointHitRadius: 50,
+      pointBorderWidth: 2,
+      data: [10000, 30162, 26263, 18394, 18287, 28682, 31274, 33259, 25849, 24159, 32651, 31984, 38451],
+    }],
+  },
+  options: {
+    scales: {
+      xAxes: [{
+        time: {
+          unit: 'date'
+        },
+        gridLines: {
+          display: false
+        },
+        ticks: {
+          maxTicksLimit: 7
+        }
+      }],
+      yAxes: [{
+        ticks: {
+          min: 0,
+          max: 40000,
+          maxTicksLimit: 5
+        },
+        gridLines: {
+          color: "rgba(0, 0, 0, .125)",
+        }
+      }],
+    },
+    legend: {
+      display: false
+    }
+  }
+});
---
+++ Day11/js/practise.js
@@ -0,0 +1,32 @@
+var slides = document.querySelectorAll('#slides > img');
+var prev = document.getElementById('prev')
+var next = document.getElementById('next')
+
+var current = 0;
+
+showSlides(current);
+// showSlides -> showSlide 로 바꼈네
+prev.onclick = prevSlide;
+next.onclick = nextSlide;
+
+console.log(current);
+
+function showSlides(n) {
+    for (var i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[n].style.display = "block";
+
+}
+
+function prevSlide() {
+    if (current > 0) current -= 1;
+    else current = slides.length - 1;
+    showSlides(current);
+}
+
+function nextSlide() {
+    if (current < slides.length - 1) current += 1
+    else current = 0;
+    showSlides(current);
+}
